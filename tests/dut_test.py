b0550341e3a88a4895d8fcde00be674e98a7d6ea
Coooolix <89058145+Coooolix@users.noreply.github.com>
2023-08-20 17:42:23 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,74 +1,93 @@
+import cocotb
+from cocotb.triggers import RisingEdge, FallingEdge, Timer
 from cocotb.drivers import BusDriver
-from cocotb.triggers import RisingEdge
-
-class InputBusDriver(BusDriver):
-    def __init__(self, clk, reset, read, write, pause, delay=0):
-        super().__init__(clk, reset)
-        self.read = read
-        self.write = write
-        self.pause = pause
-        self.delay = delay
-
-    async def update(self):
-        if RisingEdge(self.clk):
-            if not self.reset:
-                if self.read:
-                    await self.read.trigger(delay=self.delay)
-                if self.write:
-                    await self.write.trigger(delay=self.delay)
-                if self.pause:
-                    await self.pause.trigger(delay=self.delay)
-
-class OutputBusDriver(BusDriver):
-    def __init__(self, clk, reset, data, delay=0):
-        super().__init__(clk, reset)
-        self.data = data
-        self.delay = delay
-
-    async def update(self):
-        if RisingEdge(self.clk):
-            if not self.reset:
-                await self.data.write(delay=self.delay)
-
-class ConfigurationBusDriver(BusDriver):
-    def __init__(self, clk, reset, address, data, write, delay=0):
-        super().__init__(clk, reset)
-        self.address = address
-        self.data = data
-        self.write = write
-        self.delay = delay
-
-    async def update(self):
-        if RisingEdge(self.clk):
-            if not self.reset:
-                await self.address.write(delay=self.delay)
-                await self.data.write(delay=self.delay)
-                await self.write.write(delay=self.delay)
+from cocotb.coverage import CoverPoint, CoverCross
+from cocotb.monitors import BusMonitor
 
 
+class HBMController(object):
+    def __init__(self, dut):
+        self.dut = dut
+        self.sys_clk = dut.sys_clk
+        self.hb_clk = dut.hb_clk
+        self.rwds = dut.rwds
+        self.buffer = [0] * 1024
+        self.read_buffer = [0] * 1024
+        self.count = 0
+        self.start = 0
+        self.start_clk = 0
+        self.CSNeg_tmp = 1
+        self.sys_clk_period = 500
+        self.period_cnt = freq / sys_clk_period
+        self.pause = 0
+        self.pause_count = 0
+        self.rwds_sel = 0
+        self.rwds_tmp = 0
+        self.latency = 6
+        self.reset_neg = 1
+        self.rwds_in = 0
 
-class IO_Monitor(BusMonitor):
-    def __init__(self, clk, reset, data, address, write, pause):
-        super().__init__(clk, reset, data, address, write, pause)
-        self.data_recv = []
-        self.address_recv = []
-        self.write_recv = []
-        self.pause_recv = []
-        self.transaction_states = {
-            "IDLE": 0,
-            "READ": 1,
-            "WRITE": 2,
-            "PAUSE": 3
-        }
+        self.driver = BusDriver(dut, "Dout", self.sys_clk)
+        self.monitor = BusMonitor(dut, "Dout", self.sys_clk)
 
-    async def update(self):
-        if RisingEdge(self.clk):
-            if not self.reset:
-                if self.write.value:
-                    self.transaction_states["WRITE"] += 1
-                else:
-                    self.transaction_states["READ"] += 1
-                self.data_recv.append(self.data.value)
-                self.address_recv.append(self.address.value)
-                self.write_recv.append(self.write.value)
-                self.pause_recv.append(self.pause.value)
+        @cocotb.coroutine
+        def reset_sequence():
+            yield RisingEdge(self.sys_clk)
+            yield self.dut.reset_neg.eq(1)
+            yield RisingEdge(self.sys_clk)
+            yield self.dut.reset_neg.eq(0)
+            yield RisingEdge(self.sys_clk)
+
+        @cocotb.coroutine
+        def test_sequence():
+            while True:
+                yield RisingEdge(self.start)
+                cp_data = CoverPoint(self.driver.recv(), bins=range(256))
+                cp_data.sample()
+                yield RisingEdge(self.sys_clk)
+                self.count = self.count + 1
+                if self.count == end_count:
+                    self.start = 0
+
+        @cocotb.coroutine
+        def input_driver():
+            while True:
+                yield RisingEdge(self.sys_clk)
+                yield self.driver.send(self.buffer[self.count])
+                yield RisingEdge(self.sys_clk)
+                self.count = self.count + 1
+                if self.count == end_count:
+                    self.start = 0
+
+        @cocotb.coroutine
+        def output_driver():
+            while True:
+                yield RisingEdge(self.start)
+                yield self.driver.send(self.buffer[self.count])
+                yield RisingEdge(self.sys_clk)
+                self.count = self.count + 1
+                if self.count == end_count:
+                    self.start = 0
+
+        @cocotb.coroutine
+        def config_driver():
+            while True:
+                yield RisingEdge(self.sys_clk)
+                yield self.driver.send(self.buffer[self.count])
+                yield RisingEdge(self.sys_clk)
+                self.count = self.count + 1
+                if self.count == end_count:
+                    self.start = 0
+
+        @cocotb.coroutine
+        def io_monitor():
+            while True:
+                yield RisingEdge(self.sys_clk)
+                yield self.monitor.recv()
+
+        cocotb.run_until(reset_sequence())
+        cocotb.fork(test_sequence())
+        cocotb.fork(input_driver())
+        cocotb.fork(output_driver())
+        cocotb.fork(config_driver())
+        cocotb.fork(io_monitor())

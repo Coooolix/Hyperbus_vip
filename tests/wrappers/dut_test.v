ab2b7c6e61bc2758b2a15d10f4f607013327ab7b
Coooolix <89058145+Coooolix@users.noreply.github.com>
2023-09-03 17:13:33 +0530

    Update dut_test.v

--- tests/wrappers/dut_test.v
+++ tests/wrappers/dut_test.v
@@ -15,3 +15,44 @@ module s27kl0642_test
 	CKn		 ,
     RESETNeg
     );
+ inout  DQ7;
+    inout  DQ6;
+    inout  DQ5;
+    inout  DQ4;
+    inout  DQ3;
+    inout  DQ2;
+    inout  DQ1;
+    inout  DQ0;
+    inout  RWDS;
+
+    input  CSNeg;
+    input  CK;
+	input  CKn;
+    input  RESETNeg;
+
+s27kl0642 u1(
+	.DQ7(DQ7)      ,
+	.DQ6(DQ6)      ,
+	.DQ5(DQ5)      ,
+	.DQ4(DQ4)      ,
+	.DQ3(DQ3)      ,
+	.DQ2(DQ2)      ,
+	.DQ1(DQ1)      ,
+	.DQ0(DQ0)      ,
+	.RWDS(RWDS)     ,
+
+	.CSNeg(CSNeg)    ,
+	.CK(CK)       ,
+	.CKn(CKn)		 ,
+	.RESETNeg(RESETNeg)
+    );
+
+ initial begin
+      $dumpfile("dut.vcd");
+      $dumpvars;
+      CLK=0;
+   forever begin
+      #5 CLK=~CLK;
+      end
+   end
+   endmodule

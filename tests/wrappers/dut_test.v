53cc99bf819029f70a97ebb2185e145c2c3a7c93
Coooolix <89058145+Coooolix@users.noreply.github.com>
2023-09-03 17:01:45 +0530

    Update dut_test.v

--- tests/wrappers/dut_test.v
+++ tests/wrappers/dut_test.v
@@ -1,1 +1,14 @@
+`timescale 1ps/1ps
 
+module hbram_ctrl_test(
+);
+
+  initial begin
+      $dumpfile("dut.vcd");
+      $dumpvars;
+      CLK=0;
+   forever begin
+      #5 CLK=~CLK;
+      end
+   end
+   endmodule

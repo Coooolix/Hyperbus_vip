11c44bdd11f812bccfc42fcec86f2890a90cfb27
Coooolix <89058145+Coooolix@users.noreply.github.com>
2023-09-07 19:21:29 +0530

    Update dut_init.py

--- tests/dut_init.py
+++ tests/dut_init.py
@@ -1,38 +1,31 @@
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, Timer
-from cocotb.binary import BinaryValue
+from cocotb.triggers import RisingEdge, Timer
 
-@cocotb.test()
-async def test_dut_init(dut):
-    # Initialize clock
-    clock = Clock(dut.CK, 5, units="ns")  # 200MHz
-    cocotb.fork(clock.start())
-
+async def dut_init(dut):
     # Initialize signals
     dut.CSNeg <= 1
     dut.CK <= 0
     dut.CKn <= 1
-    dut.RESETNeg <= 1
-    dut.RWDS <= 0
-    dut.DQ7 <= BinaryValue("zzzzzzzz")
-    dut.DQ6 <= BinaryValue("zzzzzzzz")
-    dut.DQ5 <= BinaryValue("zzzzzzzz")
-    dut.DQ4 <= BinaryValue("zzzzzzzz")
-    dut.DQ3 <= BinaryValue("zzzzzzzz")
-    dut.DQ2 <= BinaryValue("zzzzzzzz")
-    dut.DQ1 <= BinaryValue("zzzzzzzz")
-    dut.DQ0 <= BinaryValue("zzzzzzzz")
-
-    # Wait for a few clock cycles
-    for i in range(10):
-        await RisingEdge(dut.CK)
-
-    # Assert RESETNeg
     dut.RESETNeg <= 0
-    await Timer(10, units="ns")
+    dut.DQ7 <= 0
+    dut.DQ6 <= 0
+    dut.DQ5 <= 0
+    dut.DQ4 <= 0
+    dut.DQ3 <= 0
+    dut.DQ2 <= 0
+    dut.DQ1 <= 0
+    dut.DQ0 <= 0
+    dut.RWDS <= 0
+
+    # Apply reset signal
+    dut.RESETNeg <= 1
+    await Timer(10, units='ns')
+    dut.RESETNeg <= 0
+    await Timer(10, units='ns')
     dut.RESETNeg <= 1
 
-    # Deassert CSNeg
-    dut.CSNeg <= 0
-    await Timer(10, units="ns")
+    # Wait for reset to complete
+    await RisingEdge(dut.CK)
+    while dut.RESETNeg.value == 0:
+        await RisingEdge(dut.CK)
+    await Timer(1, units='us')
